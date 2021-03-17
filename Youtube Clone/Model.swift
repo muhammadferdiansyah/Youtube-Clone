c5f49e34f957d2c7d145d8272badd3f4dff7b3e7
muhammadferdiansyah <dandyferdiansyah2801@gmail.com>
2021-03-17 14:04:45 +0700

    Hari ke tiga - Menampilkan data ke TableView

--- Youtube Clone/Model.swift
+++ Youtube Clone/Model.swift
@@ -7,17 +7,32 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    
+    func videoFatchead(_ videos: [Video])
+}
+
 class Model {
+    
+    var delegate: ModelDelegate?
+    
+    // buat fungsi untuk ngambil data dari youtube API
     func getVideos(){
+        // simpan url ke dalam varibael
         let url = URL (string: Constants.API_URL)
         
+        // kita cek urlnya kosong gak?
         guard url != nil else{
             return
         }
         
+        // mendapatkan URLSession dari object
         let session = URLSession.shared
+        
+        // mendapatkan data dari URLSession
         let dataTask = session.dataTask(with: url!){ (data, response, error) in
             
+            // cek kalau ada error
             if error != nil || data == nil{
                 return
             }
@@ -27,6 +42,12 @@ class Model {
                 decoder.dateDecodingStrategy = .iso8601
                 
                 let response = try decoder.decode(Response.self, from: data!)
+                
+                if response.items != nil{
+                    DispatchQueue.main.async {
+                        self.delegate?.videoFatchead(response.items!)
+                    }
+                }
                 
                 dump(response)
             }
